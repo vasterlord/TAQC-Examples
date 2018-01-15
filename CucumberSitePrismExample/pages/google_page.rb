b3075a3eadcdf2c5598b2fa440dd70cd375200b2
vasterlord <vasterlord619@gmail.com>
2018-01-15 17:42:19 +0200

    some change

--- CucumberSitePrismExample/pages/google_page.rb
+++ CucumberSitePrismExample/pages/google_page.rb
@@ -5,17 +5,21 @@ class GooglePage < SitePrism::Page
    set_url "http://www.google.com" 
 
    element :search_input, "//input[@name='q']" 
-   element :search_button, "//input[@name='btnK']" 
+   element :search_button, "//input[@name='btnK']"  
+   elements :search_results, "//h3[@class='r']"
 
   def search_data(search_text)   
     search_input.set search_text 
     search_button.click 
-    LoggerWrapper.info 'Data searching...' 
-    puts search_text 
-    quit
+    LoggerWrapper.logger.info 'Data searching...' 
+    puts search_text  
+    search_results.each do |item| 
+    	puts item.text
+    end 
+    search_results.size
   end 
 end 
  
 page = GooglePage.new 
 page.load 
-page.search_data('data')
+page.search_data('America') 
--- CucumberSitePrismExample/support/env.rb
+++ CucumberSitePrismExample/support/env.rb
@@ -3,7 +3,7 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'test_site' 
 require 'capybara/dsl'
-
+ 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("--window-size=1920,1080") 
@@ -23,9 +23,34 @@ Capybara.register_driver :headless_chrome do |app|
     browser: :chrome, 
     options: options 
   )
-end
+end  
 
-Capybara.default_driver = :chrome
-Capybara.javascript_driver = :chrome
-Capybara.current_driver = :chrome
+Capybara.register_driver :firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.add_argument("--window-size=1920,1080") 
+  Selenium::WebDriver::Firefox.driver_path = "lib/geckodriver.exe"
+  Capybara::Selenium::Driver.new(app,
+    browser: :firefox, 
+    options: options 
+  )
+end 
  
+Capybara.register_driver :headless_firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.add_argument("--window-size=1920,1080") 
+  options.add_argument('--headless')  
+  Selenium::WebDriver::Firefox.driver_path = "lib/geckodriver.exe"
+  Capybara::Selenium::Driver.new(app,
+    browser: :firefox, 
+    options: options 
+  )
+end 
+ 
+Capybara.configure do |config|
+  Capybara.default_driver = :firefox
+  Capybara.javascript_driver = :headless_firefox
+  Capybara.current_driver = :firefox
+  config.run_server = false
+  config.default_selector = :xpath
+  config.default_max_wait_time = 5
+end 
